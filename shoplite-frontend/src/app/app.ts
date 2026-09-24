c9cff314b57a0ebf89961790fe112cd1b3dcdb43
uvsq22307000 <sabine.challal@yahoo.com>
2026-09-24 13:35:24 +0200

    Ajout affichage nom utilisateur et bouton deconnexion dans la barre de navigation

--- shoplite-frontend/src/app/app.ts
+++ shoplite-frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { Panier } from './services/panier';
+import { Auth } from './services/auth';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,14 @@ import { Panier } from './services/panier';
 export class App {
   protected readonly title = signal('shoplite-frontend');
 
-  constructor(public panierService: Panier) {}
+  constructor(
+    public panierService: Panier,
+    public authService: Auth,
+    private router: Router
+  ) {}
+
+  seDeconnecter(): void {
+    this.authService.deconnexion();
+    this.router.navigate(['/catalogue']);
+  }
 }
